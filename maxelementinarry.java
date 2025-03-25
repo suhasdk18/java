8ef7989ed43efb0942a60d2b7aed930ff2506293
suhasdk18 <suhasdk18@gmail.com>
2025-03-25 19:29:13 +0530

    Create maxelementinarry.java

---
+++ maxelementinarry.java
@@ -0,0 +1,29 @@
+package daaa;
+import java.util.Scanner;
+
+public class MaxElementInArray {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        int[] array = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int max = array[0];
+        for (int i = 1; i < n; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+        System.out.println("The maximum element in the array is: " + max);
+
+        scanner.close();
+    }
+}
